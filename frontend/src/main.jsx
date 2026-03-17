771629c6f05549b267d83fee649132ef2c3115ed
Kevin <kjostes95@gmail.com>
2026-03-17 11:31:07 -0500

    add new basename for routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Movie-Favorite-App/">
       <App />
     </BrowserRouter>
   </StrictMode>,
